49763ca5f8c11ed6dd2a157d4033df63c334c46f
orlambda <115938017+orlambda@users.noreply.github.com>
2025-12-21 22:41:24 +0000

    Add a set of thread ids to check for duplicates

--- concurrency/00 dynamic threads/main.cpp
+++ concurrency/00 dynamic threads/main.cpp
@@ -10,24 +10,42 @@
 // Threads do not print until all threads have been created.
 
 // 2025-12-21
-// I wrote this code before watching the next video in the series: https://www.youtube.com/watch?v=kfLE57ljoEE,
-// which basically does the same thing.
-// I've now added std::this_thread::get_id() demonstrate that each thread is unique.
-// Renamed thread_id to thread_index to avoid ambiguity
+/*
+    I wrote the original code before watching the next video in the series: https://www.youtube.com/watch?v=kfLE57ljoEE,
+    which basically does the same thing.
+    I updated the code seeing the video use std::this_thread::get_id().
+    Threads now print their id to demonstrate that each thread is unique.
+    Renamed thread_id to thread_index to avoid ambiguity
+    Added set of thread ids to check for duplicates
+*/
 
 #include <iostream>
 #include <thread>
+#include <unordered_set>
 #include <vector>
+
+// Global variables for ease
+std::unordered_set<std::thread::id> ids;
+std::atomic<bool> duplicate_id_found = false;
 
 void print_from_thread(int thread_index, std::atomic<int>* current_thread)
 {
+    // Wait until this is the current thread
     while (thread_index != *current_thread)
-    {
-        // Wait until this is the current thread
-    }
+        {}
     // threads[0] is the second thread after main thread
     std::cout << "Printing from thread " << thread_index + 2 << "." << std::endl;
-    std::cout << "Thread id: " << std::this_thread::get_id() << std::endl;
+    auto id = std::this_thread::get_id();
+    std::cout << "Thread id: " << id << std::endl;
+    if (ids.contains(id))
+    {
+        std::cout << "Duplicate id found." << std::endl;
+        duplicate_id_found = true;
+    }
+    else
+    {
+        ids.insert(id);
+    }
 }
 
 int main()
@@ -44,6 +62,8 @@ int main()
 
     std::cout << "Printing from main thread." << std::endl;
     std::cout << "Thread id: " << std::this_thread::get_id() << std::endl;
+    ids.insert(std::this_thread::get_id());
+
 
     // Include main thread in thread count
     for (int i = 0; i < total_threads + -1; ++i)
@@ -52,6 +72,7 @@ int main()
         // threads.push_back(std::thread(&print_from_thread, i, &current_thread));
     }
 
+    // Tell each thread to print
     // We care about the vector index, not the thread's id
     for (int i = 0; i < threads.size(); ++i)
     {
@@ -61,6 +82,16 @@ int main()
     }
 
     std::cout << "Printing complete!" << std::endl;
+    std::cout << "Total number of threads: " << total_threads << std::endl;
+    std::cout << "Total number of thread ids: " << ids.size() << std::endl;
+    if (duplicate_id_found)
+    {
+        std::cout << "Duplicate thread id(s) were found." << std::endl;
+    }
+    else
+    {
+        std::cout << "No duplicate thread ids were found." << std::endl;
+    }
 
     return 0;
 }
